110cc5b005b2445f610426fcf07fa0958ec3112e
JOJUNHEE <eh7457303j@gmail.com>
2021-04-11 18:20:46 +0900

    History CRUD API 구현
    PetId에 따른 list와 MemberId에 따른 list를 얻는 GetAPI 구현.

---
+++ mungmunglogServer/Controllers/HistoryApiController.cs
@@ -0,0 +1,222 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using mungmunglogServer.Data;
+using mungmunglogServer.Models;
+
+namespace mungmunglogServer.Controllers
+{
+    [Route("api/history")]
+    //[Authorize]
+    [ApiController]
+    public class HistoryApiController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public HistoryApiController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/history/list/pet/{petId}
+        [HttpGet("list/pet/{petId}")]
+        public async Task<ActionResult<ListResponse<HistoryDto>>> GetPetHistories(int petId)
+        {
+            var histories = await _context.History
+                .Where(h => h.PetId == petId)
+                .Select(h => new HistoryDto(h))
+                .ToListAsync();
+
+            if (histories == null)
+            {
+                return new ListResponse<HistoryDto>
+                {
+                    Code = Models.StatusCode.NotFound,
+                    Message = "Not Found Historys"
+                };
+            }
+
+            return new ListResponse<HistoryDto>
+            {
+                Code = Models.StatusCode.Ok,
+                Message = "Success Get Histories",
+                List = histories
+            };
+        }
+
+        // GET: api/history/list/member/{memberId}
+        [HttpGet("list/member/{petId}")]
+        public async Task<ActionResult<ListResponse<HistoryDto>>> GetMemberHistories(int memberId)
+        {
+            var histories = await _context.History
+                .Where(h => h.FamilyMemberId == memberId)
+                .Select(h => new HistoryDto(h))
+                .ToListAsync();
+
+            if (histories == null)
+            {
+                return new ListResponse<HistoryDto>
+                {
+                    Code = Models.StatusCode.NotFound,
+                    Message = "Not Found Historys"
+                };
+            }
+
+            return new ListResponse<HistoryDto>
+            {
+                Code = Models.StatusCode.Ok,
+                Message = "Success Get Histories",
+                List = histories
+            };
+        }
+
+        // GET: api/history/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SingleResponse<HistoryDto>>> GetHistory(int id)
+        {
+            var history = await _context.History.FindAsync(id);
+
+            if (history == null)
+            {
+                return new SingleResponse<HistoryDto>
+                {
+                    Code = Models.StatusCode.NotFound,
+                    Message = "Not Found Historys"
+                };
+            }
+
+            return new SingleResponse<HistoryDto>
+            {
+                Code = Models.StatusCode.Ok,
+                Message = "Success Get Histories",
+                Data = new HistoryDto(history)
+            };
+        }
+
+        // PUT: api/history/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<ActionResult<CommonResponse>> PutHistory(int id, HistoryPutModel model)
+        {
+            var history = await _context.History.Where(h => h.HistoryId == id).FirstOrDefaultAsync();
+
+            if (history == null)
+            {
+                return new CommonResponse
+                {
+                    Code = Models.StatusCode.NotFound,
+                    Message = "Not Found The History"
+                };
+            }
+
+            history.Type = model.Type;
+            history.Date = new DateTime((long)model.Date * 10_000_000, DateTimeKind.Utc);
+            history.Contents = model.Contents;
+            history.FileUrl1 = model.FileUrl1;
+            history.FileUrl2 = model.FileUrl2;
+            history.FileUrl3 = model.FileUrl3;
+            history.FileUrl4 = model.FileUrl4;
+            history.FileUrl5 = model.FileUrl5;
+
+            _context.Entry(history).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+
+                return new CommonResponse
+                {
+                    Code = Models.StatusCode.Ok,
+                    Message = "Success To Put History "
+                };
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!HistoryExists(id))
+                {
+                    return new CommonResponse
+                    {
+                        Code = Models.StatusCode.NotFound,
+                        Message = "Not Found The History"
+                    };
+                } else
+                {
+                    return new CommonResponse
+                    {
+                        Code = Models.StatusCode.Unknown,
+                        Message = "Unknown Error"
+                    };
+                }
+                
+            }
+        }
+
+        // POST: api/history
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<SingleResponse<HistoryDto>>> PostHistory(HistoryPostModel model)
+        {
+            var newHistory = new History
+            {
+                Type = model.Type,
+                Date = new DateTime((long)model.Date * 10_000_000, DateTimeKind.Utc),
+                Contents = model.Contents,
+                Deleted = false,
+                FileUrl1 = model.FileUrl1,
+                FileUrl2 = model.FileUrl2,
+                FileUrl3 = model.FileUrl3,
+                FileUrl4 = model.FileUrl4,
+                FileUrl5 = model.FileUrl5,
+                PetId = model.PetId,
+                FamilyMemberId = model.FamilyMemberId
+            };
+
+
+            _context.History.Add(newHistory);
+            await _context.SaveChangesAsync();
+
+            return new SingleResponse<HistoryDto>
+            {
+                Code = Models.StatusCode.Ok,
+                Message = "Success Post",
+                Data = new HistoryDto(newHistory)
+            };
+        }
+
+        // DELETE: api/history/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<CommonResponse>> DeleteHistory(int id)
+        {
+            var history = await _context.History.FindAsync(id);
+            if (history == null)
+            {
+                return new CommonResponse
+                {
+                    Code = Models.StatusCode.NotFound,
+                    Message = "Not Found The History"
+                };
+            }
+
+            history.Deleted = true;
+            await _context.SaveChangesAsync();
+
+            return new CommonResponse
+            {
+                Code = Models.StatusCode.Ok,
+                Message = "Success To Delete History "
+            };
+        }
+
+        private bool HistoryExists(int id)
+        {
+            return _context.History.Any(e => e.HistoryId == id);
+        }
+    }
+}
--- mungmunglogServer/Models/History.cs
+++ mungmunglogServer/Models/History.cs
@@ -48,4 +48,59 @@ namespace mungmunglogServer.Models
         public FamilyMember FamilyMember { get; set; }
 
     }
+
+    public class HistoryDto
+    {
+        public HistoryDto(History history)
+        {
+            DateTime baseTime = new DateTime(2001, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            DateTime date = history.Date;
+
+            long dateTick = date.Ticks - baseTime.Ticks;
+            TimeSpan dateTimeSpan = new TimeSpan(dateTick);
+
+            HistoryId = history.HistoryId;
+            Type = history.Type;
+            Date = dateTimeSpan.TotalSeconds;
+            Contents = history.Contents;
+            Deleted = history.Deleted;
+            FileUrl1 = history.FileUrl1;
+            FileUrl2 = history.FileUrl2;
+            FileUrl3 = history.FileUrl3;
+            FileUrl4 = history.FileUrl4;
+            FileUrl5 = history.FileUrl5;
+        }
+
+        public int HistoryId { get; set; }
+        public int Type { get; set; }
+        public double Date { get; set; }
+        public string Contents { get; set; }
+        public bool Deleted { get; set; }
+        public string FileUrl1 { get; set; }
+        public string FileUrl2 { get; set; }
+        public string FileUrl3 { get; set; }
+        public string FileUrl4 { get; set; }
+        public string FileUrl5 { get; set; }
+
+        public int PetId { get; set; }
+        public int FamilyMemberId { get; set; }
+    }
+}
+
+public class HistoryPutModel
+{
+    public int Type { get; set; }
+    public double Date { get; set; }
+    public string Contents { get; set; }
+    public string FileUrl1 { get; set; }
+    public string FileUrl2 { get; set; }
+    public string FileUrl3 { get; set; }
+    public string FileUrl4 { get; set; }
+    public string FileUrl5 { get; set; }
+}
+
+public class HistoryPostModel: HistoryPutModel
+{
+    public int PetId { get; set; }
+    public int FamilyMemberId { get; set; }
 }
